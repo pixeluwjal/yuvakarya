9bd967f84730cbbd69d89d5dbdcde09a49f9ca71
pixeluwjal <easynetcraft@gmail.com>
2025-09-15 23:26:52 +0530

    web

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
-import type { NextConfig from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Disable TypeScript and ESLint checks during the build
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // Use this option for server-side-only packages in the App Router
   experimental: {
     serverComponentsExternalPackages: ['googleapis', 'google-auth-library'],
   },
